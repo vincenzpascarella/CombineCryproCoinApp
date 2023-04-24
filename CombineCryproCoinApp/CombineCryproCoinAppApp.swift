9e05b7af937280bc8652691c7a73a000e6484c99
Vincenzo Pascarella <vincenz.pascarella@gmail.com>
2023-04-24 17:30:44 +0200

    Initial Commit

---
+++ CombineCryproCoinApp/CombineCryproCoinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineCryproCoinAppApp.swift
+//  CombineCryproCoinApp
+//
+//  Created by Vincenzo Pascarella on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineCryproCoinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
